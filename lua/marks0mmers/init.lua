47c2c29a051ec8579aa731c192db38d72162511a
Mark Sommers <mark_sommers@apple.com>
2024-08-14 14:26:25 -0400

    Restructured Layout

---
+++ lua/marks0mmers/init.lua
@@ -0,0 +1,11 @@
+require('marks0mmers.set')
+require('marks0mmers.remap')
+require('marks0mmers.lazy_init')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
