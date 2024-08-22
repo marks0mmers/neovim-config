19d7aa0e5a1a7bdbc9f984026d3c7a3bafa4b185
Mark Sommers <mark_sommers@apple.com>
2024-08-21 20:31:43 -0400

    Added undotree

--- lua/marks0mmers/remap.lua
+++ lua/marks0mmers/remap.lua
@@ -3,3 +3,4 @@ vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = '[E]xplorer' })
