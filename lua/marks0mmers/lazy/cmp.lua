eea058bc272837b3fa7eca1475b73d00817d0537
Mark Sommers <mark_sommers@apple.com>
2024-08-27 16:00:44 -0400

    Updated cmp and conform configs

--- lua/marks0mmers/lazy/cmp.lua
+++ lua/marks0mmers/lazy/cmp.lua
@@ -5,7 +5,7 @@ return {
     {
       'L3MON4D3/LuaSnip',
       build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+        if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
           return
         end
         return 'make install_jsregexp'
@@ -14,29 +14,43 @@ return {
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
+    'onsails/lspkind.nvim',
   },
   config = function()
-    local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+    local lspkind = require('lspkind')
+    luasnip.config.setup({})
 
-    cmp.setup {
+    cmp.setup({
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
+      ---@diagnostic disable-next-line: missing-fields
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol_text',
+          menu = {
+            buffer = '[Buffer]',
+            nvim_lsp = '[LSP]',
+            luasnip = '[LuaSnip]',
+            nvim_lua = '[Lua]',
+          },
+        }),
+      },
       completion = { completeopt = 'menu,menuone,noinsert' },
-      mapping = cmp.mapping.preset.insert {
+      mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
-        ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<C-Space>'] = cmp.mapping.complete({}),
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
@@ -47,7 +61,7 @@ return {
             luasnip.jump(-1)
           end
         end, { 'i', 's' }),
-      },
+      }),
       sources = {
         {
           name = 'lazydev',
@@ -57,6 +71,6 @@ return {
         { name = 'luasnip' },
         { name = 'path' },
       },
-    }
+    })
   end,
 }
--- lua/marks0mmers/lazy/conform.lua
+++ lua/marks0mmers/lazy/conform.lua
@@ -12,24 +12,27 @@ return {
       desc = '[F]ormat buffer',
     },
   },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      html = { 'prettierd' },
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      typescript = { 'prettierd', 'prettier', stop_after_first = true },
-      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      go = { 'gofumpt', 'goimports' },
-      rust = { 'rustfmt' },
-    },
-  },
+  opts = function()
+    local js_formatters = { 'prettierd', 'prettier', stop_after_first = true }
+    return {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = {}
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        html = { 'prettierd' },
+        javascript = js_formatters,
+        javascriptreact = js_formatters,
+        typescript = js_formatters,
+        typescriptreact = js_formatters,
+        go = { 'gofumpt', 'goimports', 'golines' },
+        rust = { 'rustfmt' },
+      },
+    }
+  end,
 }
