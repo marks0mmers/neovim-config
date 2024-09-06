fdd1579067442c32fc6d2dbda56d20aa7f59f504
Mark Sommers <mrksommers64@gmail.com>
2024-09-05 22:06:32 -0400

    Updated LSP to rename to TS

--- lua/marks0mmers/lazy/lsp.lua
+++ lua/marks0mmers/lazy/lsp.lua
@@ -196,6 +196,10 @@ return {
       require('mason-lspconfig').setup({
         handlers = {
           function(server_name)
+            -- https://github.com/neovim/nvim-lspconfig/pull/3232
+            if server_name == 'tsserver' then
+              server_name = 'ts_ls'
+            end
             local server = servers[server_name] or {}
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
