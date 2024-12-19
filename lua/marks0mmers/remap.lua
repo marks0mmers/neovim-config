a77e8da81d0295e1bc77c199010479bec622efa2
Mark Sommers <mark_sommers@apple.com>
2024-12-18 22:34:40 -0500

    Added oil.nvim

--- lua/marks0mmers/remap.lua
+++ lua/marks0mmers/remap.lua
@@ -3,7 +3,9 @@ vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = '[E]xplorer' })
+vim.keymap.set('n', '<leader>e', function()
+  require('oil').open()
+end, { desc = '[E]xplorer' })
 vim.keymap.set('v', 'J', "<cmd>m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', "<cmd>m '<-2<CR>gv=gv")
 
