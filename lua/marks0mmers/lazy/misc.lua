508203fc0f7c9c2e2c8236184efac1c6a68ad7ad
Mark Sommers <mark_sommers@apple.com>
2024-12-19 23:52:42 -0500

    Added treesj

--- lua/marks0mmers/lazy/misc.lua
+++ lua/marks0mmers/lazy/misc.lua
@@ -127,4 +127,13 @@ return {
       require('package-info').setup()
     end,
   },
+  {
+    'Wansmer/treesj',
+    keys = { '<space>m', '<space>j', '<space>s' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+    config = function()
+      require('treesj').setup({--[[ your config ]]
+      })
+    end,
+  },
 }
