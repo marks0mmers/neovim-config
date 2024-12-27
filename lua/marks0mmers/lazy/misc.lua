4bc2bc9f76d68a26a721eea18fbcf7b82e371409
Mark Sommers <mark_sommers@apple.com>
2024-12-26 23:33:36 -0500

    Modularized mini.nvim and added snacks notifs

--- lua/marks0mmers/lazy/misc.lua
+++ lua/marks0mmers/lazy/misc.lua
@@ -58,20 +58,17 @@ return {
       signs = false,
     },
   },
+  { 'echasnovski/mini.ai', opts = { n_lines = 500 } },
+  { 'echasnovski/mini.surround' },
   {
-    'echasnovski/mini.nvim',
-    config = function()
-      require('mini.ai').setup({ n_lines = 500 })
-
-      require('mini.surround').setup()
-
-      local statusline = require('mini.statusline')
-      statusline.setup({ use_icons = vim.g.have_nerd_font })
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-    end,
+    'echasnovski/mini.statusline',
+    opts = { use_icons = vim.g.have_nerd_font },
+  },
+  {
+    'folke/snacks.nvim',
+    opts = {
+      notifier = {},
+    },
   },
   {
     'folke/which-key.nvim',
