ec72c72ac86d4ee393c4e4c7428d56ca9b015f38
Mark Sommers <mark_sommers@apple.com>
2025-01-11 17:46:26 -0500

    Changed from ts_ls to vtsls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,7 +34,7 @@ return {
           'gopls',
           'pyright',
           'rust_analyzer',
-          'ts_ls',
+          'vtsls',
           'tailwindcss',
           'ols',
           'jsonls',
